98e134039fd82adb0bde8d37740dd88ba4dbbfbf
chanjin5212 <chanjin5212@gmail.com>
2022-04-28 08:49:10 +0900

    백준

--- baekjoon/src/baekjoon/Test.java
+++ baekjoon/src/baekjoon/Test.java
@@ -2,6 +2,8 @@ package baekjoon;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class Test {
 	
@@ -9,12 +11,31 @@ public class Test {
 		
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 		
-		String a = reader.readLine().toUpperCase();
+		int n = Integer.parseInt(reader.readLine());
 		
-		if (a.equals("N")) {
-			System.out.println("Naver D2");
-		} else {
-			System.out.println("Naver Whale");
+		String list[] = reader.readLine().split(" ");
+		ArrayList<Integer> list1 = new ArrayList<Integer>();
+		for (int i=0; i<list.length; i++) {
+			list1.add(Integer.parseInt(list[i]));
 		}
+		
+		boolean yn[] = new boolean[Collections.max(list1)+1];
+		yn[0] = true;
+		yn[1] = true;
+		
+		for (int i=2; i<Math.sqrt(Collections.max(list1)); i++) {
+			for (int j=i*i; j<Collections.max(list1)+1; j+=i) {
+				yn[j] = true;
+			}
+		}
+		
+		int count = 0;
+		for (int i=0; i<n; i++) {
+			if (!yn[list1.get(i)]) {
+				count++;
+			}
+		}
+		System.out.println(count);
+
 	}
 }
