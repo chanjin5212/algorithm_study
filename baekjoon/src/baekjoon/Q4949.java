259a752478177ae4943a24eca9faf6cc42bd0d9c
chanjin5212 <chanjin5212@gmail.com>
2022-05-05 13:14:37 +0900

    백준

--- baekjoon/src/baekjoon/Q4949.java
+++ baekjoon/src/baekjoon/Q4949.java
@@ -1,11 +1,51 @@
 package baekjoon;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
 //https://www.acmicpc.net/problem/4949
 public class Q4949 {
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 		
+		while (true) {
+			Stack<String> stack = new Stack<String>();
+			String s = reader.readLine();
+			if (s.charAt(0) == '.') {
+				break;
+			}
+			for (int i=0; i<s.length(); i++) {
+				if (s.charAt(i) == '.' && stack.size() == 0) {
+					System.out.println("yes");
+					break;
+				} else if (s.charAt(i) == '[' || s.charAt(i) == '(') {
+					stack.push(String.valueOf(s.charAt(i)));
+				} else if (s.charAt(i) == ')') {
+					if (stack.size() == 0 || !stack.peek().equals("(")) {
+						System.out.println("no");
+						break;
+					} else {
+						stack.pop(); 
+					}
+				} else if (s.charAt(i) == ']') {
+					if (stack.size() == 0 || !stack.peek().equals("[")) {
+						System.out.println("no");
+						break;
+					} else {
+						stack.pop();
+					}
+					
+				} else if (s.charAt(i) == '.' && stack.size() != 0) {
+					System.out.println("no");
+					break;
+				}
+			}
+			
+		}
 		
 	}
 
--- baekjoon/src/baekjoon/Test.java
+++ baekjoon/src/baekjoon/Test.java
@@ -4,38 +4,13 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 
 public class Test {
 	
 	public static void main(String[] args) throws Exception {
 		
-		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 		
-		int n = Integer.parseInt(reader.readLine());
-		
-		String list[] = reader.readLine().split(" ");
-		ArrayList<Integer> list1 = new ArrayList<Integer>();
-		for (int i=0; i<list.length; i++) {
-			list1.add(Integer.parseInt(list[i]));
-		}
-		
-		boolean yn[] = new boolean[Collections.max(list1)+1];
-		yn[0] = true;
-		yn[1] = true;
-		
-		for (int i=2; i<Math.sqrt(Collections.max(list1)); i++) {
-			for (int j=i*i; j<Collections.max(list1)+1; j+=i) {
-				yn[j] = true;
-			}
-		}
-		
-		int count = 0;
-		for (int i=0; i<n; i++) {
-			if (!yn[list1.get(i)]) {
-				count++;
-			}
-		}
-		System.out.println(count);
-
+ 
 	}
 }
